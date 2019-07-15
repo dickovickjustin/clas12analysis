6a6d847055e4f9b1dabf76d9292f69b54afd4e97
AngelaBi <abiselli@fairfield.edu>
2019-07-15 11:13:50 -0400

    adding bin with bash script for fast execution and a new main program to test vectors

--- analysisTools/src/main/java/org/funp/testVectors.java
+++ analysisTools/src/main/java/org/funp/testVectors.java
@@ -1,13 +1,24 @@
 package org.funp;
+//---- imports for GROOT library
+import org.jlab.groot.data.*;
+import org.jlab.groot.graphics.*;
+//---- imports for PHYSICS library
+import org.jlab.jnp.physics.*;
+import org.jlab.jnp.reader.*;
 
 /**
- * Hello world!
+ * testing LorentzVector
  *
  */
-public class App 
+public class testVectors
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LorentzVector v1=new LorentzVector();
+        LorentzVector v2=new LorentzVector();
+        v1.setPxPyPzM(1, 0, 0, 0.5);
+        v2.setPxPyPzM(1, 1, 0, 0.5);
+
+        System.out.println( v1.vect().angle(v2.vect()) );
     }
 }
